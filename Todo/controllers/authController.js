b311cf597b3bc22a78e42b763234b9768cafd5fb
rajkumarandekar <rajkumarandekar1144@gmail.com>
2023-12-31 12:42:18 +0530

    login

--- Todo/controllers/authController.js
+++ Todo/controllers/authController.js
@@ -25,6 +25,30 @@ const authController = {
       res.status(500).json({ message: "Registration failed" });
     }
   },
+
+  login: async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+      const user = await User.findOne({ username });
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      const passwordMatch = await bcrypt.compare(password, user.password);
+      if (!passwordMatch) {
+        return res.status(401).json({ message: "Invalid credentials" });
+      }
+
+      const token = jwt.sign({ userId: user._id }, "secret-key", {
+        expiresIn: "1h",
+      });
+
+      res.status(200).json({ token });
+    } catch (error) {
+      res.status(500).json({ message: "Login failed" });
+    }
+  },
 };
 
 module.exports = authController;
