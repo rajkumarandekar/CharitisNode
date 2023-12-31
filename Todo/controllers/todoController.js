2afc8d0e1f1b5853c0d31202c6c85fd4bea88eda
rajkumarandekar <rajkumarandekar1144@gmail.com>
2023-12-31 14:03:33 +0530

    GET_ALL

--- Todo/controllers/todoController.js
+++ Todo/controllers/todoController.js
@@ -10,62 +10,6 @@ const todoController = {
       res.status(500).json({ message: "Error fetching todos" });
     }
   },
-
-  createTodo: async (req, res) => {
-    const { text } = req.body;
-
-    try {
-      const newTodo = new Todo({ text });
-      await newTodo.save();
-      res
-        .status(201)
-        .json({ message: "Todo created successfully", todo: newTodo });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Error creating todo" });
-    }
-  },
-
-  getTodoById: async (req, res) => {
-    const { id } = req.params;
-
-    try {
-      const todo = await Todo.findById(id);
-
-      if (!todo) {
-        return res.status(404).json({ message: "Todo not found" });
-      }
-
-      res.status(200).json({ todo });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Error fetching todo" });
-    }
-  },
-
-  updateTodoById: async (req, res) => {
-    const { id } = req.params;
-    const { text } = req.body;
-
-    try {
-      const updatedTodo = await Todo.findByIdAndUpdate(
-        id,
-        { text },
-        { new: true }
-      );
-
-      if (!updatedTodo) {
-        return res.status(404).json({ message: "Todo not found" });
-      }
-
-      res
-        .status(200)
-        .json({ message: "Todo updated successfully", todo: updatedTodo });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Error updating todo" });
-    }
-  },
 };
 
 module.exports = todoController;
--- Todo/routes/todoRoutes.js
+++ Todo/routes/todoRoutes.js
@@ -4,8 +4,5 @@ const router = express.Router();
 const todoController = require("../controllers/todoController");
 
 router.get("/", todoController.getAllTodos);
-router.post("/", todoController.createTodo);
-router.get("/:id", todoController.getTodoById);
-router.put("/:id", todoController.updateTodoById);
 
 module.exports = router;
